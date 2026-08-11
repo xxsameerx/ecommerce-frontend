10c0039d4b77d98748a420cdb4187df47ffc7182
Mohammad Sameer Abbas <mdsameerabbas777@gmail.com>
2026-08-12 03:01:24 +0530

    Fix env variable syntax for Create React App (REACT_APP_ prefix)

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
